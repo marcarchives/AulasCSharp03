8a698ec9489886928593be73f9068e4a3cda73c0
Marcos <marcos.cafsilva@TIT0636541W11-1>
2025-02-26 14:48:54 -0300

    Upload dos codigos da primeira aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 29;
+            string nomeCompleto = "Marcos César";
+
+            var variavelSemTipo = "Marcos";
+
+            Console.WriteLine(variavelSemTipo.GetType());
+
+            Console.WriteLine("Digite seu nome: ");
+
+            string nomeUsuario = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            string idadeUsuario = Console.ReadLine();
+
+            Console.WriteLine($"Meu nome é {nomeUsuario} e a minha idade é {idadeUsuario}");
+        }
+    }
+}
